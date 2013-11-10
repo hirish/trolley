0dcb3ec06009921649cc70af883d95999ee401f7
Henry Irish <henry.irish@gmail.com>
2013-11-09 23:05:27 -0800

    Major restructuring of the JS; added some backbone models and ensured that recipes are cached. Finally allowed a click on a recipe to add it to the eatlist

--- utils.py
+++ utils.py
@@ -4,4 +4,5 @@ fb = firebase.FirebaseApplication(settings.FIREBASE_URL, None)
 
 def get_recipe(recipe_id):
     recipe = fb.get('/recipe/' + str(recipe_id), None)
+    recipe['id'] = recipe_id
     return recipe
